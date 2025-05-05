a81592e77fdda12a6cf11aa6c1291aea06ca6505
BlueCool12 <pmini12@icloud.com>
2025-05-05 17:28:41 +0900

    20250505_api 개발용 경로

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: "https://bluecool.pyomin.com/api",
-    baseURL: "https://localhost:8888/api",
+    baseURL: "http://localhost:8888/api",
     headers: {
         "Content-Type": "application/json"
     },
