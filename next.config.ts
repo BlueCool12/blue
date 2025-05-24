7b121eed8c55eb89bcedf0d95e9eac79a7eca1b5
BlueCool12 <pmini12@icloud.com>
2025-05-24 11:43:47 +0900

    test5

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,14 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
   compiler: {
     styledComponents: true,
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
   }
 };
 
