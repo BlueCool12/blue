17082932590e814e2508d973f86dfe4571f78505
BlueCool12 <pmini12@icloud.com>
2025-05-24 12:07:09 +0900

    test7

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
-import path from "path";
-import type { NextConfig } from "next";
+const path = require('path');
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   compiler: {
     styledComponents: true,
   },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
-  }
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
