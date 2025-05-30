9d6a8f515a6cdadb3ba2cd4d803b62bd0ca4b7f0
BlueCool12 <pmini12@icloud.com>
2025-05-31 00:53:13 +0900

    20250531_관리자 글 수정 페이지

--- src/app/(admin)/admin/posts/[id]/edit/page.tsx
+++ src/app/(admin)/admin/posts/[id]/edit/page.tsx
@@ -1,62 +1,8 @@
-'use client';
+import dynamic from "next/dynamic";
 
-import React, { useEffect, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
-import { Post, PostForm } from "@/components/admin/PostForm";
-import { postApi } from "@/lib/api/admin/postApi";
-import { useAppDispatch } from "@/store/hooks";
-import { updatePost } from "@/store/admin/postSlice";
 
-const Edit = () => {
+const Edit = dynamic(() => import('@/components/admin/EditPostPage'), { ssr: false });
 
-    const pathname = usePathname();
-    const router = useRouter();
-    const dispatch = useAppDispatch();
-
-    const id = pathname.split('/').at(-2); // '/admin/posts/[id]/edit' → id 추출
-
-    const [postData, setPostData] = useState<Post | undefined>(undefined);
-
-    useEffect(() => {
-        if (!id) return;
-
-        const getPost = async () => {
-            try {
-                const result = await postApi.getPost(Number(id));
-                setPostData({
-                    title: result.title,
-                    content: result.content,
-                    category: result.category,
-                    isPublic: result.isPublic,
-                });
-            } catch (error) {
-                console.error('글 불러오기 실패', error);
-            }
-        };
-
-        getPost();
-    }, [id]);
-
-    const handleUpdate = (post: Post) => {
-        if (!id) return;
-
-        dispatch(updatePost({ id: Number(id), payload: post }))
-            .unwrap()
-            .then(() => {
-                router.push('/admin/posts');
-            })
-            .catch((error) => {
-                console.error('수정 실패', error);
-            });
-    };
-
-    return (
-        <PostForm
-            mode="edit"
-            initialData={postData}
-            onSubmit={handleUpdate}
-        />
-    );
+export default function Page() {
+    return <Edit />;
 }
-
-export default Edit;
