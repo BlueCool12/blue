2071d0446bfd30ff965d75a85fe64d99eef7e7cf
BlueCool12 <pmini12@icloud.com>
2025-05-31 00:14:11 +0900

    20250531_로그인 수정

--- src/lib/server/post.ts
+++ src/lib/server/post.ts
@@ -1,6 +1,7 @@
 
 export async function fetchPostBySlug(slug: string) {
-    const response = await fetch(`https://bluecool.pyomin.com/api/user/posts/${slug}`, {
+    const encodedSlug = encodeURIComponent(slug);
+    const response = await fetch(`https://bluecool.pyomin.com/api/user/posts/${encodedSlug}`, {
         cache: 'force-cache',
     });
     if (!response.ok) throw new Error('게시글이 존재하지 않습니다.');
