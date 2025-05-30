ec522ce2d1dc6e949db4f1b4948afac74e256645
BlueCool12 <pmini12@icloud.com>
2025-05-31 00:38:24 +0900

    20250531_요청 디버깅

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,34 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-    const url = req.nextUrl.clone();
-    const cookie = req.headers.get('cookie');
-    console.log('🍪 미들웨어에서 받은 쿠키:', cookie);
-    const token = req.cookies.get('token')?.value;
-    console.log('🍪 미들웨어 token 쿠키:', token);
-
-    if (url.pathname !== '/admin/login') {
-        try {
-            const response = await fetch(`https://bluecool.pyomin.com/api/auth/me`, {
-                method: 'GET',
-                headers: {
-                    cookie: `token=${token}`,
-                },
-            });
-
-            if (!response.ok) {
-                url.pathname = '/admin/login';
-                return NextResponse.redirect(url);
-            }
-        } catch {
-            url.pathname = '/admin/login';
-            return NextResponse.redirect(url);
-        }
-    }
-
-    return NextResponse.next();
+    // 추후 API 인증 토큰 갱신 처리
 }
 
 export const config = {
-    matcher: ['/admin/:path*'],
+    matcher: [],
 }
