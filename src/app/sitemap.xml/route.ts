0f401621f39ad5f0e9d5245329d8283175b457a4
BlueCool12 <pmini12@icloud.com>
2025-07-01 10:54:03 +0900

    20250701_sitemap 동적 생성

---
+++ src/app/sitemap.xml/route.ts
@@ -0,0 +1,55 @@
+import { PostListResponse } from "@/types/post";
+import { NextResponse } from "next/server";
+
+
+const BACKEND_URL = 'https://bluecool.pyomin.com';
+const FRONTEND_URL = 'https://pyomin.com';
+
+const staticUrls = [
+    '/',
+    '/about',
+    '/guestbooks'
+];
+
+async function fetchPosts() {
+    const res = await fetch(`${BACKEND_URL}/api/user/posts`);
+    return res.json();
+}
+
+export async function GET() {
+    const posts = await fetchPosts();
+
+    const dynamicUrls = posts.map((post: PostListResponse) => {
+        return `
+            <url>
+                <loc>${FRONTEND_URL}/posts/${post.slug}</loc>
+                <lastmod>${new Date(post.updatedAt).toISOString()}</lastmod>
+                <changefreq>weekly</changefreq>
+                <priority>0.8</priority>
+            </url>`
+    }).join('');
+
+    const staticPart = staticUrls.map((path) => {
+        const priority = path === '/' ? '1.0' : '0.5';
+        return `
+            <url>
+                <loc>${FRONTEND_URL}${path}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+                <changefreq>monthly</changefreq>
+                <priority>${priority}</priority>
+            </url>`
+    }).join('');
+
+    const xml = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${staticPart}
+            ${dynamicUrls}
+        </urlset>`;
+
+    return new NextResponse(xml, {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/xml',
+        },
+    });
+};
