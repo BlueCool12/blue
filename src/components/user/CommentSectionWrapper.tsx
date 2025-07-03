02ec4cffeae08b836cd80bdbe08f6abc4659f106
BlueCool12 <pmini12@icloud.com>
2025-07-03 18:14:48 +0900

    20250703_CommentSectionWrapper 수정

--- src/components/user/CommentSectionWrapper.tsx
+++ src/components/user/CommentSectionWrapper.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from 'next/dynamic';
 import { LoadingSpinner } from '@/components/common/LoadingSpinner';
-import { useEffect, useState } from 'react';
 
 interface Props {
     postId: number;
@@ -10,17 +9,12 @@ interface Props {
 
 const CommentSection = dynamic<Props>(
     () => import('./CommentSection').then(mod => mod.CommentSection),
-    { ssr: false }
+    {
+        ssr: false,
+        loading: () => <LoadingSpinner />,
+    }
 );
 
 export default function CommentSectionWrapper({ postId }: Props) {
-    const [mounted, setMounted] = useState(false);
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) return <LoadingSpinner />;
-
     return <CommentSection postId={postId} />;
 }
