eae24bb3bff338a240abdcf3bbab77e8c7dad37c
BlueCool12 <pmini12@icloud.com>
2025-08-01 00:38:28 +0900

    20250801_쿠키 설정 변경

--- src/lib/utils/session.ts
+++ src/lib/utils/session.ts
@@ -10,5 +10,5 @@ export const ensureSessionId = () => {
     if (existing) return;
 
     const newId = uuidv4();
-    document.cookie = `${cookieName}=${newId}; path=/; max-age=2592000; SameSite=None; Secure`;
+    document.cookie = `${cookieName}=${newId}; path=/; max-age=2592000; domain=.pyomin.com; SameSite=None; Secure`;
 }
