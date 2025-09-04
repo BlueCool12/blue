24ae43cd1c9adb0daf95d853e2c2ee22793c42a3
BlueCool12 <pmini12@icloud.com>
2025-09-05 00:30:07 +0900

    fix: manifest 및 카테고리 이미지 추가
    - manifest용 이미지를 사이즈별로 추가
    - 카테고리 추가에 따른 이미지 추가

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -18,7 +18,16 @@ export default function manifest(): MetadataRoute.Manifest {
         lang: 'ko-KR',
         categories: ['programming', 'blog', 'technology', 'education'],
         icons: [
-            { src: '/favicon.ico' },
+            { src: "/images/manifest/icon-192.png", sizes: "192x192", type: "image/png", purpose: "any" },
+            { src: "/images/manifest/icon-192-maskable.png", sizes: "192x192", type: "image/png", purpose: "maskable" },
+            { src: "/images/manifest/icon-512.png", sizes: "512x512", type: "image/png", purpose: "any" },
+            { src: "/images/manifest/icon-512-maskable.png", sizes: "512x512", type: "image/png", purpose: "maskable" },
+
+            { src: "/images/manifest/icon-256.png", sizes: "256x256", type: "image/png" },
+            { src: "/images/manifest/icon-384.png", sizes: "384x384", type: "image/png" },
+
+            { src: "/images/manifest/icon-32.png", sizes: "32x32", type: "image/png" },
+            { src: "/images/manifest/icon-16.png", sizes: "16x16", type: "image/png" },
         ],
         shortcuts: [
             { name: 'Posts', url: '/posts', description: '전체 글 목록' },
