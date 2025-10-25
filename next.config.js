58765a6bcc712e76949ebef8f62dc8d3085abecd
BlueCool12 <pmini12@icloud.com>
2025-10-26 01:58:11 +0900

    chore(build): 사용하지 않는 모듈 정리

--- next.config.js
+++ next.config.js
@@ -7,8 +7,6 @@ const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
 });
 
-const { getApiBase } = require('@/lib/api/apiBase');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
