4365db11b5824c969a2ca2934661ca22343b84ae
BlueCool12 <pmini12@icloud.com>
2025-10-29 18:24:57 +0900

    chore(ci): getApiBase 수정

--- src/lib/api/apiBase.ts
+++ src/lib/api/apiBase.ts
@@ -1,25 +1,11 @@
 
 export function getApiBase(): string {
-
+    
     const isServer = typeof window === 'undefined';
-    const preferPublic = process.env.PREFER_PUBLIC_API === '1';
 
-    if (process.env.DEBUG_API_BASE === '1') {
-        console.error('[getApiBase]', {
-            isServer,
-            preferPublic,
-            INTERNAL: process.env.INTERNAL_API_BASE_URL,
-            PUBLIC: process.env.PUBLIC_API_BASE_URL,
-            NEXT_PUBLIC: process.env.NEXT_PUBLIC_API_BASE_URL,
-            NODE_ENV: process.env.NODE_ENV,
-        });
+    if (isServer && process.env.INTERNAL_API_BASE_URL) {
+        return process.env.INTERNAL_API_BASE_URL;
     }
 
-    if (preferPublic) {
-        return process.env.PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL!;
-    }
-    if (isServer) {
-        return process.env.INTERNAL_API_BASE_URL || process.env.PUBLIC_API_BASE_URL!;
-    }
-    return process.env.NEXT_PUBLIC_API_BASE_URL!;
+    return process.env.NEXT_PUBLIC_API_BASE_URL || process.env.PUBLIC_API_BASE_URL!;
 }
