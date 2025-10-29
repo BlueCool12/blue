8d42785c75578e2b21c9a3d859ed9703622317ef
BlueCool12 <pmini12@icloud.com>
2025-10-29 17:11:14 +0900

    fix(build): apiBase.ts 수정

--- src/lib/api/apiBase.ts
+++ src/lib/api/apiBase.ts
@@ -2,11 +2,15 @@
 export function getApiBase(): string {
 
     const isServer = typeof window === 'undefined';
-    const isBuildTime = process.env.PREFER_PUBLIC_API === '1';
+    const preferPublic = process.env.PREFER_PUBLIC_API === '1';
 
-    if (isBuildTime) return process.env.INTERNAL_API_BASE_URL!;
+    if (preferPublic) {
+        return process.env.PUBLIC_API_BASE_URL || process.env.NEXT_PUBLIC_API_BASE_URL!;
+    }
 
-    if (isServer) return process.env.INTERNAL_API_BASE_URL!;
+    if (isServer) {
+        return process.env.INTERNAL_API_BASE_URL || process.env.PUBLIC_API_BASE_URL!;
+    }
 
     return process.env.NEXT_PUBLIC_API_BASE_URL!;
 }
